ec836efe62dd6bcdf353a2f7ef2d9899cc869c78
curryzxh <curryzxh@foxmail.com>
2026-05-01 13:55:46 +0800

    dev

--- samples/Luna.Mobile.Sample/Views/ButtonDemoView.axaml.cs
+++ samples/Luna.Mobile.Sample/Views/ButtonDemoView.axaml.cs
@@ -1,12 +1,11 @@
 using Avalonia.Controls;
-using System;
 using Luna.Mobile.Sample.ViewModels;
 
 namespace Luna.Mobile.Sample.Views;
 
 public partial class ButtonDemoView : UserControl
 {
-        private ButtonDemoViewModel ViewModel { get; } = new();
+    private ButtonDemoViewModel ViewModel { get; } = new();
 
     public ButtonDemoView()
     {
--- samples/Luna.Mobile.Sample/Views/MainView.axaml.cs
+++ samples/Luna.Mobile.Sample/Views/MainView.axaml.cs
@@ -1,5 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.Interactivity;
 using Luna.Mobile.Sample.ViewModels;
 
 namespace Luna.Mobile.Sample.Views;
@@ -14,30 +13,86 @@ public partial class MainView : UserControl
         DataContext = ViewModel;
         CatalogPage.DataContext = ViewModel;
         ViewModel.CatalogItemRequested += OnCatalogItemRequested;
+        ShellNavigation.CurrentPageChanged += OnCurrentPageChanged;
+        UpdateHeader();
     }
 
-    private void OnCatalogItemRequested(object? sender, CatalogItemViewModel item)
+    private async void OnCatalogItemRequested(object? sender, CatalogItemViewModel item)
     {
-        var view = CreateSampleView(item.Path);
-        if (view is null)
+        var page = CreateSamplePage(item);
+        if (page is null)
         {
             return;
         }
 
-        ShellHost.Page = new ContentPage
+        await ShellNavigation.PushAsync(page);
+    }
+
+    private async void BackButton_OnClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (ShellNavigation.CanGoBack)
+        {
+            await ShellNavigation.PopAsync();
+        }
+    }
+
+    private void OnCurrentPageChanged(object? sender, System.EventArgs e)
+    {
+        UpdateHeader();
+    }
+
+    private void UpdateHeader()
+    {
+        var currentPage = ShellNavigation.CurrentPage;
+        var isCatalogPage = currentPage is null || ReferenceEquals(currentPage, CatalogPage);
+
+        HeaderBar.IsVisible = !isCatalogPage;
+        BackButton.IsVisible = !isCatalogPage && ShellNavigation.CanGoBack;
+        HeaderTitleText.Text = isCatalogPage ? string.Empty : GetHeaderText(currentPage?.Header);
+    }
+
+    private static string GetHeaderText(object? header)
+    {
+        return header switch
+        {
+            null => string.Empty,
+            string text => text,
+            TextBlock textBlock => textBlock.Text ?? string.Empty,
+            _ => header.ToString() ?? string.Empty,
+        };
+    }
+
+    private static Page? CreateSamplePage(CatalogItemViewModel item)
+    {
+        var view = CreateSampleView(item.Path);
+        if (view is null)
+        {
+            return null;
+        }
+
+        if (view is Page page)
+        {
+            NavigationPage.SetHasNavigationBar(page, false);
+            NavigationPage.SetHasBackButton(page, false);
+
+            if (page.Header is null || page.Header is string { Length: 0 })
+            {
+                page.Header = item.Name;
+            }
+
+            return page;
+        }
+
+        var wrappedPage = new ContentPage
         {
             Header = item.Name,
             AutomaticallyApplySafeAreaPadding = true,
             Content = view,
         };
 
-        BackButton.IsVisible = true;
-    }
-
-    private void BackButton_OnClick(object? sender, RoutedEventArgs e)
-    {
-        ShellHost.Page = CatalogPage;
-        BackButton.IsVisible = false;
+        NavigationPage.SetHasNavigationBar(wrappedPage, false);
+        NavigationPage.SetHasBackButton(wrappedPage, false);
+        return wrappedPage;
     }
 
     private static Control? CreateSampleView(string path)

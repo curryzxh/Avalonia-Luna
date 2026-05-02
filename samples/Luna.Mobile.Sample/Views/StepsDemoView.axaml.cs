9ef4480c519934c0ffb64a9ca6bcd8d531ffe36f
curryzxh <curryzxh@foxmail.com>
2026-05-02 21:40:58 +0800

    dev

--- samples/Luna.Mobile.Sample/Views/StepsDemoView.axaml.cs
+++ samples/Luna.Mobile.Sample/Views/StepsDemoView.axaml.cs
@@ -1,15 +1,53 @@
+using System.Collections.ObjectModel;
+using System.Globalization;
 using Avalonia.Controls;
-using Luna.Mobile.Sample.ViewModels;
+using Avalonia.Interactivity;
+using Luna.Mobile.Controls;
 
 namespace Luna.Mobile.Sample.Views;
 
 public partial class StepsDemoView : UserControl
 {
-    private StepsDemoViewModel ViewModel { get; } = new();
+    private const int SpecialStepsMaxCount = 6;
+    private readonly ObservableCollection<StepItem> _specialInteractiveItems = [];
+    private int _specialCurrentIndex = 3;
+    private int _specialStepCount = 4;
 
     public StepsDemoView()
     {
         InitializeComponent();
-        DataContext = ViewModel;
+        SpecialInteractiveSteps.ItemsSource = _specialInteractiveItems;
+        RebuildSpecialInteractiveSteps();
+    }
+
+    private void OnSpecialNextStepClick(object? sender, RoutedEventArgs e)
+    {
+        if (_specialStepCount < SpecialStepsMaxCount)
+        {
+            _specialCurrentIndex = _specialStepCount;
+            _specialStepCount++;
+        }
+        else if (_specialCurrentIndex < _specialStepCount - 1)
+        {
+            _specialCurrentIndex++;
+        }
+
+        RebuildSpecialInteractiveSteps();
+    }
+
+    private void RebuildSpecialInteractiveSteps()
+    {
+        _specialInteractiveItems.Clear();
+
+        for (var index = 0; index < _specialStepCount; index++)
+        {
+            _specialInteractiveItems.Add(new StepItem
+            {
+                Title = $"步骤 {index + 1}",
+                TitleRight = ">",
+            });
+        }
+
+        SpecialInteractiveSteps.Current = _specialCurrentIndex.ToString(CultureInfo.InvariantCulture);
     }
 }

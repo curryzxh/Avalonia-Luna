3d350c96c1b1167d37e2a999d3246170cba38e55
curryzxh <curryzxh@foxmail.com>
2026-05-08 14:04:44 +0800

    dev

--- samples/Luna.Desktop.Sample/ViewModels/Samples/PopupSampleViewModel.cs
+++ samples/Luna.Desktop.Sample/ViewModels/Samples/PopupSampleViewModel.cs
@@ -1,3 +1,4 @@
+using Avalonia.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -9,6 +10,48 @@ public partial class PopupSampleViewModel()
     [ObservableProperty]
     private bool isPopupOpen;
 
+    [ObservableProperty]
+    private bool isTopOpen;
+
+    [ObservableProperty]
+    private bool isBottomOpen;
+
+    [ObservableProperty]
+    private bool isLeftOpen;
+
+    [ObservableProperty]
+    private bool isRightOpen;
+
+    [ObservableProperty]
+    private bool isCenterOpen;
+
+    [ObservableProperty]
+    private PlacementMode selectedPlacement = PlacementMode.Bottom;
+
+    public PlacementMode[] Placements { get; } =
+    {
+        PlacementMode.Bottom,
+        PlacementMode.Top,
+        PlacementMode.Left,
+        PlacementMode.Right,
+        PlacementMode.Center,
+    };
+
     [RelayCommand]
     private void TogglePopup() => IsPopupOpen = !IsPopupOpen;
+
+    [RelayCommand]
+    private void ToggleTop() => IsTopOpen = !IsTopOpen;
+
+    [RelayCommand]
+    private void ToggleBottom() => IsBottomOpen = !IsBottomOpen;
+
+    [RelayCommand]
+    private void ToggleLeft() => IsLeftOpen = !IsLeftOpen;
+
+    [RelayCommand]
+    private void ToggleRight() => IsRightOpen = !IsRightOpen;
+
+    [RelayCommand]
+    private void ToggleCenter() => IsCenterOpen = !IsCenterOpen;
 }
